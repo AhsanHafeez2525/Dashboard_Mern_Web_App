8c1d47f512996064db49429a769f831e0654d3df
Ahsan <ahsansatti402@gmail.com>
2023-11-26 01:39:57 +0500

    make-db_user-signupApi_dataSave_mongodb

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,24 @@
 const express = require('express');
-
-const mongoose = require('mongoose');
-
+require('./db/config');
+const User = require('./db/User');
 const app = express();
 
-const connectDB = async () => {
-	mongoose.connect('mongodb://localhost:27017/e-commerce');
-	const productSchema = new mongoose.Schema({});
-	const products = mongoose.model('products', productSchema);
-	const data = await products.find();
-	console.log(data);
-};
-connectDB();
+app.use(express.json());
+
+app.post('/register', async (req, res) => {
+	let user = new User(req.body);
+	let result = await user.save();
+	// res.send(req.body);
+	res.send(result);
+});
+
+// const connectDB = async () => {
+// 	const productSchema = new mongoose.Schema({});
+// 	const products = mongoose.model('products', productSchema);
+// 	const data = await products.find();
+// 	console.log(data);
+// };
+// connectDB();
 
 // app.get('/', (req, res) => {
 // 	res.send('welcome to mern stack course');
