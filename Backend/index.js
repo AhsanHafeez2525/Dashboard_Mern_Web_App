0294400b62f4d9d0fa0f5aea090ae875eaef97da
Ahsan <ahsansatti402@gmail.com>
2023-11-26 01:16:01 +0500

    add_mongodb_mongoose

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('welcome to mern stack course');
-});
+const connectDB = async () => {
+	mongoose.connect('mongodb://localhost:27017/e-commerce');
+	const productSchema = new mongoose.Schema({});
+	const products = mongoose.model('products', productSchema);
+	const data = await products.find();
+	console.log(data);
+};
+connectDB();
+
+// app.get('/', (req, res) => {
+// 	res.send('welcome to mern stack course');
+// });
 
 app.listen(5000, () => {
 	console.log('server is running');
