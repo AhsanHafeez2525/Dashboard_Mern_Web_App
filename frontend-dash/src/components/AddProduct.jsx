5184e4d2bfac9e3692b207ecc783c5589ee26105
Ahsan <ahsansatti402@gmail.com>
2023-12-04 00:36:04 +0500

    add-product_validation

--- frontend-dash/src/components/AddProduct.jsx
+++ frontend-dash/src/components/AddProduct.jsx
@@ -5,8 +5,14 @@ const AddProduct = () => {
 	const [price, setPrice] = useState('');
 	const [category, setCategory] = useState('');
 	const [company, setCompany] = useState('');
+	const [error, setError] = useState(false);
 
 	const addProduct = async () => {
+		if (!name || !price || !category || !company) {
+			setError(true);
+			return false;
+		}
+
 		console.warn(name, price, category, company);
 		const userId = JSON.parse(localStorage.getItem('user'))._id;
 		console.log(userId);
@@ -38,6 +44,16 @@ const AddProduct = () => {
 					setName(e.target.value);
 				}}
 			/>
+			{error && !name && (
+				<span
+					style={{
+						color: 'red',
+						marginLeft: 24,
+						fontSize: 14,
+					}}>
+					Enter valid name
+				</span>
+			)}
 			<input
 				type='text'
 				name=''
@@ -48,6 +64,16 @@ const AddProduct = () => {
 					setPrice(e.target.value);
 				}}
 			/>
+			{error && !price && (
+				<span
+					style={{
+						color: 'red',
+						marginLeft: 24,
+						fontSize: 14,
+					}}>
+					Enter valid price
+				</span>
+			)}
 			<input
 				type='text'
 				name=''
@@ -58,6 +84,16 @@ const AddProduct = () => {
 					setCategory(e.target.value);
 				}}
 			/>
+			{error && !category && (
+				<span
+					style={{
+						color: 'red',
+						marginLeft: 24,
+						fontSize: 14,
+					}}>
+					Enter valid category
+				</span>
+			)}
 			<input
 				type='text'
 				name=''
@@ -68,6 +104,16 @@ const AddProduct = () => {
 					setCompany(e.target.value);
 				}}
 			/>
+			{error && !company && (
+				<span
+					style={{
+						color: 'red',
+						marginLeft: 24,
+						fontSize: 14,
+					}}>
+					Enter valid company
+				</span>
+			)}
 			<button onClick={addProduct} className='signup-button'>
 				Add Product
 			</button>
