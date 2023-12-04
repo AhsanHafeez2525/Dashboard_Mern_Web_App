10643c840adc0ff80ec72d9aea2977fdd67e8a01
Ahsan <ahsansatti402@gmail.com>
2023-12-04 17:22:20 +0500

    intergrate-deleteApi

--- frontend-dash/src/components/ProductList.jsx
+++ frontend-dash/src/components/ProductList.jsx
@@ -13,6 +13,18 @@ const ProductList = () => {
 		}
 	};
 
+	const deleteProduct = async id => {
+		console.log(id);
+		let result = await fetch(`http://localhost:5000/products/${id}`, {
+			method: 'Delete',
+		});
+		result = await result.json();
+		if (result) {
+			// alert('record is delete');
+			getProducts();
+		}
+	};
+
 	useEffect(() => {
 		getProducts();
 	}, []);
@@ -25,19 +37,25 @@ const ProductList = () => {
 				<li>Name</li>
 				<li>Price</li>
 				<li>Category</li>
+				<li>Operation</li>
 			</ul>
-			<ul>
-				{products.map((item, index) => (
-					<li key={index}>
-						<ul>
-							<li>{index + 1}</li>
-							<li>{item.name}</li>
-							<li>${item.price}</li>
-							<li>{item.category}</li>
-						</ul>
-					</li>
-				))}
-			</ul>
+			{products.map((item, index) => (
+				<li key={index}>
+					<ul key={item._id}>
+						<li>{index + 1}</li>
+						<li>{item.name}</li>
+						<li>${item.price}</li>
+						<li>{item.category}</li>
+						<li>
+							<button
+								onClick={() => deleteProduct(item._id)}
+								className='delete-button'>
+								Delete
+							</button>
+						</li>
+					</ul>
+				</li>
+			))}
 		</div>
 	);
 };
